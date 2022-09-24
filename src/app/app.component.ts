e8d50d3c4dea775c55d14154c4eae3e61a9e0571
karthik-we <karthickpremoth@gmail.com>
2022-09-24 17:31:26 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  userForm : FormGroup;
+  listData:any;
+
+  
+  constructor(private fb:FormBuilder){
+    this.listData=[];
+    this.userForm=this.fb.group({
+      fname:['',Validators.required],
+      lname:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      dob:['',Validators.required],
+      gender:['',Validators.required],
+      address:['',Validators.required],
+      contact:['',
+      [Validators.required,
+      Validators.pattern('^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$')
+      ]]
+    })
+  
+  }
+  public addItem():void{
+    this.listData.push(this.userForm.value);
+    this.userForm.reset();
+  }
+
+  reset(){
+    this.userForm.reset();
+  }
+
+  removeItem(element: any){
+    this.listData.forEach((value: any,index: any)=>{
+      if(value==element)
+      this.listData.splice(index,1);
+    }); 
+  }
+  get fname() {
+    return this.userForm.get('fname');
+  }
+  get lname() {
+    return this.userForm.get('lname');
+  }
+  get email() {
+    return this.userForm.get('email');
+  }
+  get gender() {
+    return this.userForm.get('gender');
+  }
+  get address() {
+    return this.userForm.get('address');
+  }
+  get contact() {
+    return this.userForm.get('contact');
+  }
+  ngOnInit(){    
+  }
 }
